55fa687047d757ae7f01e8f2aa4e5048794e3c7b
Michael McCullough <michael.mccullough21@gmail.com>
2019-06-19 17:23:08 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,10 +21,10 @@ end
 def now_serving(array)
   if array.length == 0
     puts "There is nobody waiting to be served!"
-  # elsif array.length > 0
-  #      array.each do |name|
-  #      "Currently serving #{name}."
-  #    array.shift
-  #  end
+  elsif array.length > 0
+       array.each do |name|
+       puts "Currently serving #{name}."
+       array.shift
+   end
   end
 end
