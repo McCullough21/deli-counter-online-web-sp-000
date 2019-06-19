e756b5f6ebe2170d4c414b2c2265e4016ca98167
Michael McCullough <michael.mccullough21@gmail.com>
2019-06-19 15:52:33 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,9 @@
-# Write your code here.
+def line(people)
+  count = 1
+   if people.size == 0
+     "The line is currently empty"
+   elsif people.each do |name|
+     puts "Welcome, #{name}. You are number #{count} in line."
+     count += 1
+   end
+end
