1cff8e1fdafd10889ab396ee05fe5d9ff25f9b97
Michael McCullough <michael.mccullough21@gmail.com>
2019-06-19 16:32:06 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,8 +13,7 @@ end
 
 def take_a_number(array, people)
    count = 1
-   people.each do |name|
-    puts "Welcome, #{name}. You are number #{count} in line."
+    puts "Welcome, #{array[-1]}. You are number #{count} in line."
   count += 1
 end
 end
